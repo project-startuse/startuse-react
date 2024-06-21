98bffea9687094c8274e55e7b7b9e8d66285fee5
Saandeep Kotte <saandeepkotte5@gmail.com>
2024-06-21 19:58:00 +0530

    home page styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import heroImg from '../resources/hero-img.svg';
 import '../styles/Home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div className='home'>
-            <span className='info'>
-                <h1>We make BUSINESSES reach PEOPLE</h1>
-                <h1>We help you SOLVE your PROBLEMS</h1>
+            <span className='hero-section'>
+                <span>
+                    <h1>We make BUSINESSes reach PEOPLE</h1>
+                    <h1>We help you SOLVE your PROBLEMS</h1>
+                </span>
+                <span className='explore'>
+                    <Link to={'/feed'}>Explore {' ->'}</Link>
+                </span>
             </span>
             <span className='hero-image'>
                 <img src={heroImg} alt='logo' height={500} />
