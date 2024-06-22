8fcefb8c32187ef405c4518850dec0530456560d
Saandeep Kotte <saandeepkotte5@gmail.com>
2024-06-22 15:30:36 +0530

    home naviagtion for logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import startuseLogo from '../resources/startuse-logo-temp-png.png';
 import '../styles/Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
+    const navigate = useNavigate();
     return (
         <div className='navbar'>
-            <span>
-                <img src={startuseLogo} alt='logo' width={75} />
+            <span onClick={() => navigate('/')}>
+                <img src={startuseLogo} alt='logo' width={50} />
             </span>
             <span className='btn-grp'>
                 <span><Link to={'/login'} >Login</Link></span>
